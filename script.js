dfb0ada15234ad29f6c439a1d54802f828ba4d41
Tomtomprin <97924842+Tomtomprin@users.noreply.github.com>
2022-03-14 18:25:10 +0900

    NewPost

--- script.js
+++ script.js
@@ -1,25 +1,39 @@
-// ★★★↓ここがポイント★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★
 const ClassBody = document.querySelector('body');
-// ★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★
-
-
-
 const ClassOpenbtn = document.querySelector('.openbtn');
 const ClassNavigation = document.querySelector('.navigation');
+const ClassClosebtn = document.querySelector('.closebtn');
+
+// iOSでスクロールを禁止にする関数
+function disableScroll(event) {
+    event.preventDefault();
+}
 
 
 function ClickOpenbtn() {
-    ClassOpenbtn.classList.toggle('active');
-    ClassNavigation.classList.toggle('panelactive');
-    // ★★★↓ここがポイント★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★
-    ClassBody.classList.toggle('body_active');
-    // ★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★
+    ClassOpenbtn.classList.add('active');
+    ClassNavigation.classList.add('panelactive');
+    ClassBody.classList.add('body_active');
+    ClassClosebtn.style.display = 'block';
+    // ↓今回の例では分かりやすいようにクローズボタンに黄色のスタイルを当てている
+    // ClassClosebtn.style.background = 'yellow';
+    document.addEventListener('touchmove', disableScroll, {
+        passive: false
+    });
 }
 ClassOpenbtn.onclick = ClickOpenbtn;
 
+function ClickClosebtn() {
+    ClassOpenbtn.classList.remove('active');
+    ClassNavigation.classList.remove('panelactive');
+    ClassBody.classList.remove('body_active');
+    ClassClosebtn.style.display = 'none';
+    document.removeEventListener('touchmove', disableScroll, {
+        passive: false
+    });
+}
+ClassClosebtn.onclick = ClickClosebtn;
 
 
-// ★★★↓ここがポイント★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★
 // もし、 ボトムナビゲーションなど、 他の箇所を押した際のパネルが閉じる仕様の時は、
 // 以下の記述もしておく。
 
@@ -28,7 +42,10 @@ Icon = document.querySelector('.icon');
 function ClickIcon() {
     ClassOpenbtn.classList.remove('active');
     ClassNavigation.classList.remove('panelactive');
-    ClassBody.classList.remove('body_active')
+    ClassBody.classList.remove('body_active');
+    ClassClosebtn.style.display = 'none';
+    document.removeEventListener('touchmove', disableScroll, {
+        passive: false
+    });
 }
 Icon.onclick = ClickIcon;
-// ★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★
